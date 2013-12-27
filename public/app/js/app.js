a7ec7b5e6afd0f9078a65cf55668c1d3b457fe5a
Greg Besson <gregory.besson@adfab.fr>
2013-12-27 13:53:44 +0100

    With google charts directive

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -1,10 +1,18 @@
+// I bootstrap angular dynamically after the GCharts has been loaded
+google.setOnLoadCallback(function() {
+  angular.bootstrap(document.body, ['technoTopApp']);
+});
+google.load('visualization', '1', {packages: ['corechart']});
+
+
 var technoTopApp = angular.module('technoTopApp', [
   'ngRoute',
-  'technoTopControllers'
+  'technoTopControllers',
+  'technoTopDirectives'
 ]);
  
 technoTopApp.config(['$routeProvider', function($routeProvider) {
-    $routeProvider
-    		.when('/techno/:techno', {templateUrl: 'partials/techno.html', controller: 'TechnoTopCtrl'})
-    		.otherwise({redirectTo: '/'});
-  }]);
+	$routeProvider
+		.when('/techno/:techno', {templateUrl: 'partials/techno.html', controller: 'TechnoTopCtrl'})
+		.otherwise({redirectTo: '/'});
+}]);
--- public/app/js/controllers.js
+++ public/app/js/controllers.js
@@ -2,7 +2,40 @@ var technoTopControllers = angular.module('technoTopControllers', []);
  
 technoTopControllers.controller('TechnoTopCtrl', function ($scope, $routeParams, $http) {
 	$scope.techno = $routeParams.techno;
+	
+	$scope.chartTitle = "Techno usage";
+	  $scope.chartWidth = 500;
+	  $scope.chartHeight = 320;
+	  $scope.chartData = [
+	    ['Drupal',     11],
+	    ['WordPress',      2],
+	    ['Magento',  2],
+	    ['ZF2', 2],
+	    ['SF2',    7]
+	  ];
+	  
+	  $scope.deleteRow = function (index) {
+	    $scope.chartData.splice(index, 1);
+	  };
+	  $scope.addRow = function () {
+	    $scope.chartData.push([]);
+	  };
+	  $scope.selectRow = function (index) {
+	    $scope.selected = index;
+	  };
+	  $scope.rowClass = function (index) {
+	    return ($scope.selected === index) ? "selected" : "";
+	  };
+	  
+	  
 	$http.get('/techno/' + $routeParams.techno).success(function(data) {
 	    $scope.technos = data.data;
+	    $scope.chartData = [
+	                	    ['Drupal',     250],
+	                	    ['WordPress',      2],
+	                	    ['Magento',  2],
+	                	    ['ZF2', 2],
+	                	    ['SF2',    7]
+	                	  ];
 	});
 });
