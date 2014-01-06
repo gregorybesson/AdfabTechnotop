3f54fd74e367757532ea26fa2f92386ad512eaa6
Greg Besson <gregory.besson@adfab.fr>
2014-01-06 11:17:41 +0100

    Fix Unit test failing + remove unuseful graphic on techno page

--- module/Application/tests/TestConfig.php
+++ module/Application/tests/TestConfig.php
@@ -2,17 +2,23 @@
 return array(
     'modules' => array(
         'DoctrineModule',
-    	'DoctrineORMModule',
-   		'DoctrineDataFixtureModule',
-   		'ZendDeveloperTools',
-   		'Jhu\ZdtLoggerModule',
-    	'AsseticBundle',
-    	'ZfcBase',
-   		'PlaygroundCore',
+        'DoctrineORMModule',
+        'DoctrineDataFixtureModule',
+        'ZendDeveloperTools',
+        'Jhu\ZdtLoggerModule',
+        'PlaygroundTemplateHint',
+        'AsseticBundle',
+        'ZfcBase',
+        'ZfcUser',
+        'BjyAuthorize',
+        'PlaygroundCore',
+        'PlaygroundDesign',
+        'PlaygroundFaq',
         'PlaygroundUser',
-   		'PlaygroundGame',
-        'PlaygroundPartnership',
+        'PlaygroundFacebook',
+        'PlaygroundTranslate',
         'Application',
+        'AlexaTopSites'
     ),
     'module_listener_options' => array(
         'config_glob_paths'    => array(
